c9356820383a618ed815504f57abdc6cd420528b
Hmmtrnt <rento5310@gmail.com>
2023-02-09 09:42:29 +0900

    スクリーンサイズ、ウィンドウモード変更

--- Util/game.h
+++ Util/game.h
@@ -2,13 +2,21 @@
 
 namespace Game
 {
+#ifdef _DEBUG
 	// ウインドウモード設定
 	constexpr bool kWindowModo = true;
+#else
+	constexpr bool kWindowModo = false;
+#endif
 	// ウインドウ名
 	const char* const kTitleText = "ゲーム名";
 	// ウインドウサイズ
-	constexpr int kScreenWidth = 800;
-	constexpr int kScreenHeight = 700;
+	/*constexpr int kScreenWidth = 800;
+	constexpr int kScreenHeight = 700;*/
+
+	constexpr int kScreenWidth = 1920;
+	constexpr int kScreenHeight = 1080;
+
 	// カラーモード
 	constexpr int kColorDepth = 32;		// 32 or 16
 };
